481a771508d5eb64a648fcfc170c11b242adafc2
DW-wen <dwen0630@gmail.com>
2024-04-25 17:00:14 +0800

    backend test

--- public/js/front.js
+++ public/js/front.js
@@ -4,14 +4,15 @@ const getBtn = document.getElementById('get');
 const postBtn = document.getElementById('post');
 const input = document.getElementById('input');
 
-const baseURL = 'http://192.168.8.180:3000/' 
+const baseURL = '//192.168.8.128:3000/' 
 
 getBtn.addEventListener('click', getInfo);
 postBtn.addEventListener('click', postInfo);
 
 async function getInfo(e){
     e.preventDefault(); // 預防刷新葉面
-    const res = await fetch(baseURL + 'info', { // add dynamic url
+    
+    const res = await fetch(baseURL + "info", { // add dynamic url
         method: 'GET'
     });
 
--- server.js
+++ server.js
@@ -28,11 +28,24 @@ const database = new name("db.db") // 這樣會自動創建
 database.loadDatabase();
 */
 
-const express = require('express')
+
+
+
+
+
+
+
+
+
+const cors = require('cors'); // 一個前後端連接協定，詳細功能不了解，只知道可以跑
+const express = require('express');
 require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 const app = express()
-const port = process.env.PORT  || 3000 ; // 存在 dotenv，只知道可以跑，其他就不知道了
+const port =  3000 ; // 存在 dotenv，只知道可以跑，其他就不知道了
 //const portNotLocal = process.env.PORT;
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.json({limit: '1mb'}));
@@ -44,16 +57,44 @@ app.listen(port, () => {
 
 
 
-const val = {
-  info: 'hi bro',
-  sound: 'yo man'
-};
+const val = [{
+  num: 3,
+  price: 30,
+  time: {
+    hour: 1,
+    min: 1,
+    sec: 1,
+  },
+  status: 1,
+  order: 1
+  }, 
+  {
+    num: 4,
+    price: 30,
+    time: {
+      hour: 1,
+      min: 1,
+      sec: 1,
+    },
+    status: 1,
+    order: 2
+    }
+]
+;
+
+
+let test_info = JSON.stringify(val);
+
+
+
+
+
 app.get('/info', (req, res) => {
   //const {dynamic} = req.params; // 解除 object 狀態
   //const {key} = req.query; // params: jame // query = key
 
   //console.log(dynamic, key);
-    res.status(200).json(val); // 回傳
+    res.status(200).json(test_info); // 回傳
 });
 
 
@@ -67,3 +108,50 @@ app.post('/', (req, res) => {
   res.status(200).send({status: 'received'});
 });
 
+/*
+資料儲存方法
+// initialize webpage
+// front -> back
+let info_sgin = {
+  account: "207", // 包成 string
+  position: "chef", // 職位
+  password: "12345678" // string type
+};
+
+// back -> front
+let info_webpage = {
+  webpage_data: "<html></html>" 
+  // i'll response webpage for you, 
+  // you save webpage in localStorage
+};
+
+
+// sending info between front and back
+let info_order = {
+  account: "207", //class name
+  position: "position", // 職位 -> ex: 飲料製作 || 熱狗堡處理
+
+  // 餐點名稱_售價
+  expense: 40, // total cost
+  beverages: {
+    // example
+    soda_20: 1, // send me in digit
+    juice_35:2
+  },
+  dishes: {
+    // example
+    marshmellow_10: 2, // send me in digit
+    instant_oodle_35: 0 
+  },
+
+  status: "in order / finished", // not yet -> in order, finished -> finished
+  orderNumber: 1, // send me in digit
+  orderTime: {  // 先在前端將時間設定好
+    date: "2024/5/11",
+    time: "10:20",
+    estimated_finished_time: "10:25",
+    estimated_time: 5 // save in digit
+  }, 
+  
+};
+*/
